243d9b010bb8eb2ac6c6e74bb1cbb95b10a266f0
turcualexd <turcualex01@gmail.com>
2023-12-16 23:44:13 +0100

    Magnetic field tests

---
+++ Code and model/Magnetic field/untitled4.m
@@ -0,0 +1,10 @@
+clear, clc, close all;
+
+a=6371.2; % Reference radius used in IGRF
+r = a+100;
+theta = 30;
+phi = 30;
+N = 13;
+
+[X1,Y1,Z1] = magnet(r,theta,phi)
+[X2, Y2, Z2] = igrf('01/01/2020', theta, phi, r, 'geocentric')
